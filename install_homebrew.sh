4daa67fdaccce296927023a5f95d6ee984d1b03e
Sarim Mahmood <sarim.mahmood@wishabi.com>
2015-03-30 15:15:38 -0700

    added message if homebrew is already installed

--- install_homebrew.sh
+++ install_homebrew.sh
@@ -4,5 +4,7 @@
 if test ! $(which brew); then
   echo "Installing homebrew..."
   ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+else
+  echo "Homebrew is already installed!"
 fi
 
