f52f30f9d9a510a62fd39fc97d718afd6e642fc3
Sarim Mahmood <sarim.mahmood@wishabi.com>
2015-03-30 18:57:29 -0400

    formatting changes

--- setup.sh
+++ setup.sh
@@ -5,28 +5,30 @@
 #---------------------------------------------------------------------#
 
 #-----------------------OS AND SUDO AUTHERIZATION---------------------#
-echo 'Please enter the password for your local machine.'
+echo "Please enter the password for your local machine."
 USEROS=$(sudo uname)
 #---------------------------------------------------------------------#
-echo The current operating system you are using is: $USEROS
+echo "The current operating system you are using is: $USEROS"
 
 if [ "$USEROS" = "Darwin" ]; then
-    echo Installing Xcode
-    $(xcode-select --install)
-    echo Installing Homebrew
+    echo "Installing Xcode"
+    xcode-select --install
+    read -p "Please follow the instructions for installing Xcode. Once it has completed, press [Enter] key to continue..."
+    echo "Installing Homebrew"
     sh install_homebrew.sh
 fi
 
 if [ "$USEROS" = "Linux" ]; then
-    echo Installing vim
-    $(sudo apt-get -y install vim)
-    echo Installing build essentials
-    $(sudo apt-get -y install build-essential curl)
+    echo "Installing vim"
+    sudo apt-get -y install vim
+    echo "Installing build essentials"
+    sudo apt-get -y install build-essential curl
 fi
 
-echo Installing rvm
+echo "Installing rvm"
 sh install_rvm.sh
 
-echo Linking rvm to bash
-source $HOME/.rvm/scripts/rvm
+echo "Linking rvm to bash"
 echo "source $HOME/.rvm/scripts/rvm" >> ~/.bash_profile
+
+echo "Please restart your terminal in order to apply the RVM changes."
