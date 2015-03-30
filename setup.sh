744ef98b1bec832c902fe141cf9a80bbe7b064d8
Andrew Seidl <andrew.seidl@wishabi.com>
2015-03-30 16:58:38 -0400

    Fixed installs

--- setup.sh
+++ setup.sh
@@ -14,17 +14,18 @@ if [ "$USEROS" = "Darwin"]; then
     echo Installing Xcode
     $(xcode-select --install)
     echo Installing Homebrew
-    ruby -e “$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)”     
+    $(ruby -e “$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)”) 
 fi
 
 if [ "$USEROS" = "Linux"]; then
-    sudo apt-get -y install vim
+    echo Installing vim
+    $(sudo apt-get -y install vim)
     echo Installing build essentials
-    sudo apt-get -y install build-essential curl
+    $(sudo apt-get -y install build-essential curl)
 fi
 
 echo Installing rvm
-curl -sSl https://get.rvm.io | bash -s stable
+$(curl -sSl https://get.rvm.io | bash -s stable)
 
 echo Linking rvm to bash
 echo "source $HOME/.rvm/scripts/rvm" >> ~/.bash_profile
